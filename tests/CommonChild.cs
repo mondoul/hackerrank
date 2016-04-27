8245b325380bffe9324e14130e4d9ac0f111e343
Arnaud Mondoulet <arnaud.mondoulet@gmail.com>
2016-04-27 17:58:37 -0400

    Added tests

---
+++ tests/CommonChild.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using challenges;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tests
+{
+    [TestClass]
+    public class CommonChild
+    {
+        [TestMethod]
+        public void CommonChild1()
+        {
+            var result = Challenges.CommonChild("HARRY\r\nSALLY");
+            Assert.AreEqual(2, result);
+        }
+        
+        [TestMethod]
+        public void CommonChild2()
+        {
+            var result = Challenges.CommonChild("AA\r\nBB");
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void CommonChild3()
+        {
+            var result = Challenges.CommonChild("SHINCHAN\r\nNOHARAAA");
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void CommonChild4()
+        {
+            var result = Challenges.CommonChild("ABCDEF\r\nFBDAMN");
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void CommonChild5()
+        {
+            var result = Challenges.CommonChild("WEWOUCUIDGCGTRMEZEPXZFEJWISRSBBSYXAYDFEJJDLEBVHHKS\r\nFDAGCXGKCTKWNECHMRXZWMLRYUCOCZHJRRJBOAJOQJZZVUYXIC");
+            Assert.AreEqual(15, result);
+        }
+    }
+}
---
+++ tests/StringReduction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using challenges;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tests.test_files;
+
+namespace tests
+{
+    [TestClass]
+    public class StringReduction
+    {
+        [TestMethod]
+        public void StringReduction1()
+        {
+            var input = TestResources.stringreduction1;
+            var res = Challenges.StringReduction(input);
+            Assert.AreEqual(TestResources.stringreduction11, res);
+        }
+
+        [TestMethod]
+        public void StringReduction2()
+        {
+            var input = TestResources.stringreduction2;
+            var res = Challenges.StringReduction(input);
+            Assert.AreEqual(TestResources.stringreduction22, res);
+        }
+    }
+}
